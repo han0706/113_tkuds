7da5b1c766c805a7e590d5b8e0972c1893cc25f1
han0706 <hanhanbebe@gmail.com>
2025-06-26 11:50:35 +0800

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,6 +1,6 @@
 public class hello {
     public static void main(String[] var0) {
-        System.err.println("Hello World!!!!");
+        System.out.println("Hello World!!!!");
     }
         
     }
