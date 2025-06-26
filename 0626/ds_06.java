bd31ead45e5e322cefde7b82905ed133f4b841d6
han0706 <hanhanbebe@gmail.com>
2025-06-26 22:50:50 +0800

    Create ds_06.java

---
+++ 0626/ds_06.java
@@ -0,0 +1,25 @@
+import java.util.Random;
+
+public class ds_06 {
+    public static void main(String[] args) {
+        int[] numbers = new int[10];
+        Random rand = new Random();
+
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = rand.nextInt(100) + 1;
+        }
+
+        int max = numbers[0];
+        for (int i = 1; i < numbers.length; i++) {
+            if (numbers[i] > max) {
+                max = numbers[i];
+            }
+        }
+
+        System.out.print("陣列內容：");
+        for (int num : numbers) {
+            System.out.print(num + " ");
+        }
+        System.out.println("\n最大值為：" + max);
+    }
+}
