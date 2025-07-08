97902a7cb0fe7506ef8795ea62ccc4176e624cb9
han0706 <hanhanbebe@gmail.com>
2025-07-08 11:32:34 +0800

    Create ArrayDemo.java

---
+++ 0708/ArrayDemo.java
@@ -0,0 +1,13 @@
+
+public class ArrayDemo {
+   public static void main (String[] args) {
+        double[] numbers ={1.11, 2.22, 3.33, 4.44, 5.55};
+        //int[] numbers= {1,2,3,4,5};
+        for (int i=0; i< numbers.length; i++){
+        System.out.println(numbers[i]);
+        System.out.println("Index:" +i);
+        System.out.println("占用記憶體:" + (Integer.BYTES+"bytes"));
+
+      }
+   } 
+}
